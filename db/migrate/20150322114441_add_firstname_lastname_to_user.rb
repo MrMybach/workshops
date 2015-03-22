01e9ffa5530c8e964ecc22ffd7a32590a037def5
MrMybach <mybach@gmail.com>
2015-03-22 13:22:15 +0100

    Setting up devise. 22 failures left

---
+++ db/migrate/20150322114441_add_firstname_lastname_to_user.rb
@@ -0,0 +1,7 @@
+class AddFirstnameLastnameToUser < ActiveRecord::Migration
+  def change
+  	add_column :users, :firstname, :string, null: false
+  	add_column :users, :lastname, :string, null: false
+  	add_column :users, :admin, :boolean, default: false
+  end
+end
