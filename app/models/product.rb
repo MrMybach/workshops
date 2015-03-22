d08fd7b0a774e1b030fdaf16712e22139cb79cf1
MrMybach <mybach@gmail.com>
2015-03-22 13:57:01 +0100

    complex validation added. average_rating method added

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,15 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  MONEY_REGEX = /\A\d+(?:\.\d{0,2})?\z/
+
+  validates_presence_of :title, :description
+  validates :price, :presence => true,
+            :numericality => true,
+            :format => { :with => MONEY_REGEX }
+
+  def average_rating
+  	self.reviews.average(:rating).to_f
+  end
 end
