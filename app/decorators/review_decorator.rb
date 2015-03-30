be21b02f8a26bff2e98168a3bd30dbaac5553805
MrMybach <mybach@gmail.com>
2015-03-31 00:27:31 +0200

    date decorator added

--- app/decorators/review_decorator.rb
+++ app/decorators/review_decorator.rb
@@ -1,8 +1,12 @@
-class ReviewDecorator < Draper::Decorator
+itclass ReviewDecorator < Draper::Decorator
   delegate_all
 
   def author  
   	model.user.firstname + " " + model.user.lastname
+  end
+
+  def published_at
+    object.created_at.strftime("%d %b %Y")
   end
 
 end
