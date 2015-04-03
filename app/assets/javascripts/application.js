9f024c12e67504ce850eea8de3b82008a1d078cb
MrMybach <mybach@gmail.com>
2015-04-03 20:35:02 +0200

    Timer refactor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,20 @@
 //= require_tree .
 
 $(document).ready(function(){
-	setTimeout(function(){
-		$('.js-fade-out-msg').fadeOut('fast');
-	}, 7000);	
+    var count = 7,
+        counter = setInterval(timer, 1000);
+
+    $('.js-fade-out-msg').append('<small class="timer pull-right"></small>');
+
+    function timer() {
+        count = count - 1;
+
+        if (count <= 0) {
+            clearInterval(counter);
+            $('.js-fade-out-msg').fadeOut('fast');
+            return;    
+        }
+
+        $('.timer').text(count + ' sec left to close.');
+    }
 });
